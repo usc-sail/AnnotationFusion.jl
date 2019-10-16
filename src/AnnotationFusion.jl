06ec48d77fc33fd72a227df34ddf3eddd68e236b
Karel Mundnich <mundnich@usc.edu>
2019-10-16 14:30:01 -0700

    Files generated by PkgTemplates

---
+++ src/AnnotationFusion.jl
@@ -0,0 +1,5 @@
+module AnnotationFusion
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AnnotationFusion
+using Test
+
+@testset "AnnotationFusion.jl" begin
+    # Write your own tests here.
+end
